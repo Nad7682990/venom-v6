3ce1785e54e1c4814c7b545234bdb66ec0e35978
Nad7682990 <nad7682990@gmail.com>
2024-09-22 16:17:30 +0530

    Update venom.py

--- venom.py
+++ venom.py
@@ -4,8 +4,8 @@ import logging
 from aiogram import Bot
 import asyncio
 
-API_TOKEN = 'TOKEN'
-ADMIN_ID = 'YOUR ID'
+API_TOKEN = '7873303931:AAHZ1gHnJ_XNypgxh4Z-TUkEWaQH8nHPNvo'
+ADMIN_ID = '1080493130'
 MAX_RESTARTS = 5
 RESTART_PERIOD = 60  # Seconds
 
@@ -57,4 +57,4 @@ if __name__ == '__main__':
     try:
         asyncio.run(main())
     except KeyboardInterrupt:
-        logging.info("Venom script terminated by user.")
+        logging.info("Nadeem script terminated by user.")
